91038da7362021fd0be0c124857332c70a4b8006
Michael Harrington <mdharr7@gmail.com>
2024-01-31 12:11:39 +0800

    Write function to manipulate dom by adding event listeners to all list item elements, then on click, the target list items box shadow value is copied to the clipboard.

--- node-types/script.js
+++ node-types/script.js
@@ -1,1 +1,45 @@
-console.log('Script running')
+document.getElementById("copyButton").addEventListener("click", function() {
+    const text = document.getElementById("textToCopy").value;
+    navigator.clipboard.writeText(text)
+        .then(() => {
+            console.log("Text copied to clipboard");
+        })
+        .catch(err => {
+            console.error("Failed to copy text: ", err);
+        });
+});
+
+// document.getElementById("copyButton").addEventListener("click", function() {
+//     // const text = document.getElementById("textToCopy").value;
+//     const text = document.getElementById('farm').children[0].innerText
+//     navigator.clipboard.writeText(text)
+//         .then(() => {
+//             console.log("Text copied to clipboard");
+//         })
+//         .catch(err => {
+//             console.error("Failed to copy text: ", err);
+//         });
+// });
+
+const listItems = document.getElementsByTagName('li')
+for(let i = 0; i < listItems.length; i++) {
+    listItems[i].addEventListener("click", function() {
+        const style = window.getComputedStyle(this)
+        const boxShadow = style.getPropertyValue("box-shadow")
+        const text = `box-shadow: ${boxShadow}`
+        const innerText = this.children[0].innerText
+        const copiedText = "Copied!"
+        this.children[0].innerText = copiedText
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                setTimeout(() => {
+                    this.children[0].innerText = innerText
+                }, 1000)
+                console.log("Text copied")
+                
+            })
+            .catch(err => {
+                console.error("Failed to copy text: ", err)
+            })
+    })
+}
