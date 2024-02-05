0bf3d937d9ff662362300e0e53754ffd9c3d8132
Michael Harrington <mdharr7@gmail.com>
2024-02-05 20:15:51 +0800

    Complete exercise 3 in attributes lesson with self-imposed challenge requiring the use of javascript to create all of the html elements and append them to the document, use arrow functions, async/await syntax, and ternary operations.

--- attributes/exercises/exercise-3.js
+++ attributes/exercises/exercise-3.js
@@ -1,5 +1,5 @@
 document.body.style.backgroundColor = '#282828'
-document.body.style.color = '#fff'
+document.body.style.color = 'rgb(0, 183, 77)'
 /*
     1. Create a new HTML file that has the following
        Elements in it:
@@ -20,3 +20,45 @@ document.body.style.color = '#fff'
     4. Verify the changes in the DOM/Elements tab as well
        as visually in the UI
 */
+
+const header = document.createElement('h1')
+header.innerText = 'Header Text'
+document.body.appendChild(header)
+
+const resetButton = document.createElement('button')
+resetButton.innerText = 'Reset'
+resetButton.addEventListener('click', async () => {
+   await swapColors()
+   resetColor()
+})
+
+document.body.appendChild(resetButton)
+
+const swapColors = () => {
+   return new Promise((resolve) => {
+      let counter = 0
+      const intervalId = setInterval(() => {
+         const color = document.querySelector('h1').style.color
+         if(counter < 50) {
+            setColor(color)
+         } else {
+            clearInterval(intervalId)
+            resolve()
+         }
+         counter++
+      }, 50)
+   })
+}
+
+const setColor = (color) => {
+   const green = 'rgb(0, 183, 77)'
+   const yellow = 'rgb(247, 227, 172)'
+   const red = 'rgb(193, 52, 56)'
+   color === green ? document.querySelector('h1').style.color = yellow
+   : color === yellow ? document.querySelector('h1').style.color = red
+   : document.querySelector('h1').style.color = green
+}
+
+const resetColor = () => {
+   document.querySelector('h1').style.color = 'rgb(0, 183, 77)'
+}
