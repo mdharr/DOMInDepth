50a647dfc1163564529c32dc532d729c50d2b6c1
Michael Harrington <mdharr7@gmail.com>
2024-02-07 23:00:49 +0800

    Practice using immediately invoked function expression

--- manipulating-dom-elements/exercises/practice.js
+++ manipulating-dom-elements/exercises/practice.js
@@ -28,5 +28,51 @@ document.body.style.color = "#fff";
 */
 
 // IIFE (Immediately Invoked Function Expression)
+(function() {
+   function createElement(tag, props, ...children) {
+      const element = document.createElement(tag)
+      Object.entries(props).forEach(([key, value]) => {
+         if(key === 'textContent') {
+            element.textContent = value
+         } else {
+            element.setAttribute(key, value)
+         }
+      })
 
+      children.forEach(child => {
+         if(typeof child === 'string') {
+            element.append(document.createTextNode(child))
+         } else {
+            element.append(child)
+         }
+      })
+      return element
+   }
+
+   const p = createElement('p', { textContent: 'P Text' })
+   const label = createElement('label', { textContent: 'Input Label' })
+   const input = createElement('input', { type: 'text', value: 'Input text here...' })
+   const div = createElement('div', {}, label, input)
+   const li1 = createElement('li', { textContent: 'List item' })
+   const li2 = createElement('li', { textContent: 'List item' })
+   const li3 = createElement('li', { textContent: 'List item' })
+   const ul = createElement('ul', {}, li1, li2, li3)
+   const nav = createElement('nav', {}, ul)
+
+   const elements = [p, div, nav]
+
+   setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * elements.length)
+
+      Array.from(document.body.childNodes).forEach(child => {
+         if(child.nodeType === Node.ELEMENT_NODE) {
+            child.style.display = 'none'
+         }
+      })
+      if(!document.body.contains(elements[randomIndex])) {
+         document.body.append(elements[randomIndex])
+      }
+      elements[randomIndex].style.display = 'block'
+   }, 1000)
+})()
 
