2325230a363d95e452c1b9b65453a19bcd3cfb99
Michael Harrington <mdharr7@gmail.com>
2024-02-10 22:34:25 +0800

    Write functions for shallow and deep copies of arrays. Write function for reversing vowels in a string.

---
+++ styles-and-classes/reversing-vowels.js
@@ -0,0 +1,54 @@
+const reverseVowels = (s) => {
+    const shallowCopy = [...s]
+    const regex = /[aeiouAEIOU]/
+    let left = 0;
+    let right = shallowCopy.length-1;
+    while(left < right) {
+        if (regex.test(shallowCopy[left]) && regex.test(shallowCopy[right])) {
+            [shallowCopy[left], shallowCopy[right]] = [shallowCopy[right], shallowCopy[left]]
+            left++
+            right--
+        } else if (!regex.test(shallowCopy[left]) && regex.test(shallowCopy[right])) {
+            left++
+        } else if (regex.test(shallowCopy[left]) && !regex.test(shallowCopy[right])) {
+            right--
+        } else {
+            left++
+            right--
+        }
+    }
+    return shallowCopy.join('')
+}
+
+// console.log(reverseVowels('supercalifragilisticexpialidocious'))
+// console.log(reverseVowels('I went to school last Saturday to see if the staff actually went home on the weekends.'))
+
+
+let a = ['a', 'b', ['c', 'd']]
+// let b = [...a]
+// a[2][1] = 'e'
+
+// console.log(a)
+// console.log(b)
+
+// shallow copy
+
+function makeDeepCopy(arr) {
+    const result = arr.map(element => {
+        if (Array.isArray(element)) {
+            return element.slice(); // Create a copy of nested arrays
+        } else if (typeof element === 'object' && element !== null) {
+            return Object.assign({}, element); // Create a copy of nested objects
+        } else {
+            return element; // For primitive types, return as is
+        }
+    });
+    
+    return result;
+}
+
+let b = makeDeepCopy(a)
+a[2][0] = 'w'
+console.log(a)
+console.log(b)
+
