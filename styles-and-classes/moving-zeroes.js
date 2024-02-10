768a1767746975844022a29b98a862e654a36780
Michael Harrington <mdharr7@gmail.com>
2024-02-10 19:22:22 +0800

    Write new functions on the array prototype chain to recreate the reverse method. Use two solutions, one creates a shallow copy, the other creates a deep copy.

--- styles-and-classes/moving-zeroes.js
+++ styles-and-classes/moving-zeroes.js
@@ -30,17 +30,88 @@
 //     return nums
 // }
 
-const moveZeroes = function(nums) {
-    let left = 0;
-    for(let right = 0; right < nums.length; right++){
-       if(nums[right] !== 0){
-           [nums[left], nums[right]] = [nums[right], nums[left]] 
-           left++;
-       }
+// const moveZeroes = function(nums) {
+//     let left = 0;
+//     for(let right = 0; right < nums.length; right++){
+//        if(nums[right] !== 0){
+//            [nums[left], nums[right]] = [nums[right], nums[left]] 
+//            left++;
+//        }
+//     }
+//    return nums
+// };
+
+const moveZeroes = (nums) => {
+    let left = 0
+    for(let right = 0; right < nums.length; right++) {
+        if(nums[right] !== 0) {
+            [nums[left], nums[right]] = [nums[right], nums[left]]
+            left++
+        }
     }
-   return nums
-};
+    return nums
+}
   
-console.log(moveZeroes([0, 1, 0, 3, 12, 0, 100, 28, 0, 26]))
+// console.log(moveZeroes([0, 1, 0, 3, 12, 0, 100, 28, 0, 26]))
 // console.log(moveZeroes([0, 0, 1]))
 // console.log(moveZeroes([0]))
+
+
+
+// let a = 1;
+// let b = 2;
+// let c = 11;
+// let d = 4;
+
+// [a, b, c, d] = [d, c, b, a]
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+
+
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ['a', 'b']]
+        
+// shallow copy
+Array.prototype.swapLetters = function() {
+    const result = [...this]
+    let left = 0
+    let right = result.length-1
+    while(left < right) {
+        [result[left], result[right]] = [result[right], result[left]]
+        left++
+        right--
+    }
+    return result
+}
+
+const shallowCopy = alphabet.swapLetters()
+
+shallowCopy[0][0] = 'Y'
+console.log(alphabet)
+console.log(shallowCopy)
+
+
+// deep copy
+Array.prototype.swapLetters = function() {
+    const result = this.map(element => {
+        if (Array.isArray(element)) {
+            return element.slice(); // Create a copy of nested arrays
+        } else if (typeof element === 'object' && element !== null) {
+            return Object.assign({}, element); // Create a copy of nested objects
+        } else {
+            return element; // For primitive types, return as is
+        }
+    });
+
+    let left = 0;
+    let right = result.length - 1;
+    while (left < right) {
+        [result[left], result[right]] = [result[right], result[left]];
+        left++;
+        right--;
+    }
+    return result;
+}
