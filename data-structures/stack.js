363eed9b4a7032ffc30444a1dbf22e4e5946799f
Michael Harrington <mdharr7@gmail.com>
2024-04-11 21:30:24 +0800

    Implement a basic Stack class in JavaScript.

---
+++ data-structures/stack.js
@@ -0,0 +1,29 @@
+class Stack {
+    constructor() {
+        this.count = 0;
+        this.storage = {};
+    }
+
+    push(value) {
+        this.storage[this.count] = value;
+        this.count++;
+    }
+
+    pop() {
+        if (this.count === 0) {
+            return undefined;
+        }
+        this.count--;
+        const result = this.storage[this.count];
+        delete this.storage[this.count];
+        return result;
+    }
+
+    peek() {
+        return this.storage[this.count - 1];
+    }
+
+    size() {
+        return this.count;
+    }
+}
