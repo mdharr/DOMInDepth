d2b997da740ed5c459407ef4485b4e9dbc77b098
Michael Harrington <mdharr7@gmail.com>
2024-04-11 21:58:35 +0800

    Practice creating Stack DS

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,18 +1,46 @@
+// class Stack {
+//     constructor() {
+//         this.count = 0;
+//         this.storage = {};
+//     }
+
+//     push(value) {
+//         this.storage[this.count] = value;
+//         this.count++;
+//     }
+
+//     pop() {
+//         if (this.count === 0) {
+//             return undefined;
+//         }
+//         this.count--;
+//         const result = this.storage[this.count];
+//         delete this.storage[this.count];
+//         return result;
+//     }
+
+//     peek() {
+//         return this.storage[this.count - 1];
+//     }
+
+//     size() {
+//         return this.count;
+//     }
+// }
+
+
 class Stack {
     constructor() {
         this.count = 0;
         this.storage = {};
     }
-
     push(value) {
         this.storage[this.count] = value;
         this.count++;
     }
 
     pop() {
-        if (this.count === 0) {
-            return undefined;
-        }
+        if (this.count === 0) return undefined;
         this.count--;
         const result = this.storage[this.count];
         delete this.storage[this.count];
@@ -27,3 +55,16 @@ class Stack {
         return this.count;
     }
 }
+
+const newStack = new Stack();
+newStack.push(1);
+newStack.push(2);
+newStack.push(3);
+newStack.push(4);
+newStack.push(5);
+newStack.pop();
+console.log(newStack);
+const peek = newStack.peek();
+console.log("Peek", peek);
+const size = newStack.size();
+console.log("Size", size);
