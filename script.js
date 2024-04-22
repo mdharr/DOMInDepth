a575f280b076fe39cfb95560bb4d58d9392f5116
Michael Harrington <mdharr7@gmail.com>
2024-04-22 18:05:15 +0800

    Solution for finding the product of an array except for self each iteration with big o of n time complexity.

--- script.js
+++ script.js
@@ -8,25 +8,51 @@
 const numArr = [1, 2, 3, 4];
 
 // O(n^2) Time Complexity
+// const findProductOfArrayExceptSelf = (nums) => {
+//     let result = [];
+//     let current = 0;
+//     let left = 0;
+//     let right = nums.length - 1;
+
+//     if (nums.length === 0) return [];
+//     if (nums.length === 1) return arr[0];
+
+//     while (left <= right) {
+//         result[left] = 1;
+//         for (let i = 0; i < nums.length; i++) {
+//             if (left === i) continue;
+//             result[left] *= nums[i];
+//             current++;
+//         }
+//         current = 0;
+//         left++;
+//     }
+//     return result;
+// }
+
 const findProductOfArrayExceptSelf = (nums) => {
-    let result = [];
-    let current = 0;
-    let left = 0;
-    let right = nums.length - 1;
+    const length = nums.length;
+    let result = new Array(length);
+    let leftProducts = new Array(length);
+    let rightProducts = new Array(length);
 
     if (nums.length === 0) return [];
-    if (nums.length === 1) return arr[0];
+    if (nums.length === 1) return nums[0];
 
-    while (left <= right) {
-        result[left] = 1;
-        for (let i = 0; i < nums.length; i++) {
-            if (left === i) continue;
-            result[left] *= nums[i];
-            current++;
-        }
-        current = 0;
-        left++;
+    leftProducts[0] = 1;
+    for (let i = 1; i < nums.length; i++) {
+        leftProducts[i] = nums[i-1] * leftProducts[i-1];
     }
+
+    rightProducts[length - 1] = 1;
+    for (let i = length - 2; i >= 0; i--) {
+        rightProducts[i] = nums[i+1] * rightProducts[i+1];
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = leftProducts[i] * rightProducts[i];
+    }
+
     return result;
 }
 
