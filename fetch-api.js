f559f190cb7aac4efe74635f3c4422c6e1b21f35
Michael Harrington <mdharr7@gmail.com>
2024-04-24 21:06:54 +0800

    Remove sensitive data

---
+++ fetch-api.js
@@ -0,0 +1,13 @@
+let dailyWeatherData = {};
+
+async function getWeatherData() {
+    const url = 'https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=';
+    const response = await fetch(url);
+    const data = await response.json();
+    dailyWeatherData = data.timelines.daily;
+    dailyWeatherData.forEach(item => {
+        console.log(`Time: ${ item.time } -> Average Tempature: ${ item.values.temperatureAvg }`);
+    });
+}
+
+getWeatherData();
